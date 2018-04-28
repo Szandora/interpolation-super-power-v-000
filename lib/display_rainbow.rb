9496c9a577d576c955aff7ccaffcdd1e525bd9e8
Sheila Wittman <szandora789@gmail.com>
2018-04-28 23:15:05 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
 # Write your #display_rainbow method here
+def display_rainbow(R, O, Y, G, B, I, V)
+  R = "red"
+  O = "Orange"
+  Y = "Yellow"
+  G = "Green"
+  B = "Blue"
+  I = "Indigo"
+  V = "Violet"
+
+  puts ("R:#{R}", "O:#{O}", "Y:#{Y}", "G:#{G}", "B:#{B}", "I:#{I}", "V:#{V}")
+end
